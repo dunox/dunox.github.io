106792a22734bc89dfc047ef47752a9eefe955c3
Andriy <34597632+dunox@users.noreply.github.com>
2019-11-10 17:11:48 +0200

    links changed

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ $(function(){
         speed: 500,
         fade: true,
         cssEase: 'linear',
-        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.png" alt=""></img>',
-        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.png" alt=""></img>',
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="app/images/arrow-left.png" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="app/images/arrow-right.png" alt=""></img>',
         dots: true,
       });
 
