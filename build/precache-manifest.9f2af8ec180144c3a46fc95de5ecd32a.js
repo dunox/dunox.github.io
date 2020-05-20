4a1f5431218941983aef09498252f090eeedff52
dunox <avshendrik@gmail.com>
2020-05-20 13:17:18 +0300

    node modules delete

---
+++ build/precache-manifest.9f2af8ec180144c3a46fc95de5ecd32a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2727a9bb09ab7b786ed67feefb35f605",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee41e1a1842b6f8ca835",
+    "url": "/static/css/main.506f44d7.chunk.css"
+  },
+  {
+    "revision": "a82a8d72428f52c25dc0",
+    "url": "/static/js/2.32bdff7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.32bdff7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee41e1a1842b6f8ca835",
+    "url": "/static/js/main.815a1c04.chunk.js"
+  },
+  {
+    "revision": "74e1cbaef1fda168aa72",
+    "url": "/static/js/runtime-main.1e79158c.js"
+  },
+  {
+    "revision": "1e58e66ef838723a0c0f915e4cb4cbf8",
+    "url": "/static/media/clock.1e58e66e.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
